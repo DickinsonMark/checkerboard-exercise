abadc8b3c70fcff2ffc22ca4bfbaa76b97136b12
Mark Dickinson <monoposon@comcast.net>
2016-07-18 15:30:55 -0600

    finished gradient

--- src/script.js
+++ src/script.js
@@ -1,19 +1,22 @@
-/////////  Random Colors
+///////////   Gradient
 
-function getRandomColor() {
-  var val = '0123456789abcdef'.split('');
-  var color = '#';
-  for (var j = 0; j < 6; j++) {
-    color += val[Math.floor(Math.random() * 16)];
+for (var i = 96; i > 33; i--) {
+
+  var block1 = document.createElement('div');
+  block1.style.backgroundColor = '#7452' + i;
+  block1.style.width = '11.1%';
+  block1.style.paddingBottom = '11.1%';
+  block1.style.float = 'left';
+
+  var block2 = document.createElement('div');
+  block2.style.backgroundColor = '#74' + i + 'a1';
+  block2.style.width = '11.1%';
+  block2.style.paddingBottom = '11.1%';
+  block2.style.float = 'left';
+
+  if (i % 2 === 0) {
+    document.body.appendChild(block1);
+  } else {
+    document.body.appendChild(block2);
   }
-  return color;
-}
-
-for (var i = 0; i < 63; i++) {
-  var block = document.createElement('div');
-  block.style.backgroundColor = getRandomColor();
-  block.style.width = '11.1%';
-  block.style.paddingBottom = '11.1%';
-  block.style.float = 'left';
-  document.body.appendChild(block);
 }
